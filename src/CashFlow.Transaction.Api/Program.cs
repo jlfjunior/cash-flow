934149cac993a98c9001302a16c7f1c85c7db53a
Jose Junior <jlfjunior8@gmail.com>
2025-10-09 09:29:27 -0300

    feat: add exception handle globally

--- src/CashFlow.Transaction.Api/Program.cs
+++ src/CashFlow.Transaction.Api/Program.cs
@@ -24,7 +24,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapTransactionEndpoints();
-
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.Run();
 
 public partial class Program { }
