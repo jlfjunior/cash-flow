8fb44c0876a9d5a52fcaa5c563ef90901d13bce7
Jose Junior <jlfjunior8@gmail.com>
2025-11-24 22:00:43 -0300

    chore: use primary constructor

--- src/CashFlow.Transactions.Api/CustomerCreatedConsumer.cs
+++ src/CashFlow.Transactions.Api/CustomerCreatedConsumer.cs
@@ -4,38 +4,30 @@ using CashFlow.Transactions.Application.Responses;
 
 namespace CashFlow.Transactions.Api;
 
-public class CustomerCreatedConsumer : IHostedService
+public class CustomerCreatedConsumer(
+    ILogger<CustomerCreatedConsumer> logger,
+    IEventBus eventBus,
+    IServiceScopeFactory scopeFactory)
+    : IHostedService
 {
-    private readonly ILogger<CustomerCreatedConsumer> _logger;
-    private readonly IEventBus _eventBus;
-    private readonly IServiceScopeFactory _scopeFactory;
-
-
-    public CustomerCreatedConsumer(ILogger<CustomerCreatedConsumer> logger, IEventBus eventBus, IServiceScopeFactory scopeFactory)
-    {
-        _logger = logger;
-        _eventBus = eventBus;
-        _scopeFactory = scopeFactory;
-    }
-
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         const string queueName = "queuing.customers.created";
         
-        await _eventBus.SubscribeAsync<CustomerCreatedResponse>(queueName, async customerCreatedResponse =>
+        await eventBus.SubscribeAsync<CustomerCreatedResponse>(queueName, async customerCreatedResponse =>
         {
             try
             {
-                await using var scope = _scopeFactory.CreateAsyncScope();
+                await using var scope = scopeFactory.CreateAsyncScope();
                 var dailyClosureService = scope.ServiceProvider.GetRequiredService<ICreateAccount>();
 
                 await dailyClosureService.ExecuteAsync(customerCreatedResponse.Id, cancellationToken);
                 
-                _logger.LogInformation("Account crated. CustomerId {Id}", customerCreatedResponse.Id);
+                logger.LogInformation("Account crated. CustomerId {Id}", customerCreatedResponse.Id);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error processing customer created event. Id {TransactionId}", customerCreatedResponse.Id);
+                logger.LogError(ex, "Error processing customer created event. Id {TransactionId}", customerCreatedResponse.Id);
                 throw;
             }
         });
